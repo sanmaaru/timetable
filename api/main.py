6d601959592088059f346b9a4d35d31275403b59
sanmaaru <180041526+sanmaaru@users.noreply.github.com>
2026-08-17 14:28:32 +0900

    🐛 fix(api): add https vercel domain to cors allowed origins

--- api/main.py
+++ api/main.py
@@ -49,7 +49,8 @@ app.add_middleware(
         "http://localhost:8000",
         "http://127.0.0.1:8000",
         "http://10.122.2.122:8000",
-        "http://timetable-six-amber.vercel.app"
+        "http://timetable-six-amber.vercel.app",
+        "https://timetable-six-amber.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
